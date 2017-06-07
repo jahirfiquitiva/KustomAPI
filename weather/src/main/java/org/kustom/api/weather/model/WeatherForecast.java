e5fb1c1475fd9ee4ba6f915cbe0fae96ede1b0ed
Frank Monza <frank.bmonza@gmail.com>
2017-06-07 17:14:01 +0200

    Added valid from and valid to fields

--- weather/src/main/java/org/kustom/api/weather/model/WeatherForecast.java
+++ weather/src/main/java/org/kustom/api/weather/model/WeatherForecast.java
@@ -9,26 +9,12 @@ public class WeatherForecast extends WeatherCondition {
     private float mTempMax = Float.MAX_VALUE;
     @SerializedName("temp_min")
     private float mTempMin = Float.MIN_VALUE;
+    @SerializedName("valid_from")
+    private long mValidFrom = 0L;
+    @SerializedName("valid_to")
+    private long mValidTo = 0L;
 
     public WeatherForecast() {
-    }
-
-    protected WeatherForecast(Parcel in) {
-        super(in);
-        mTempMax = in.readFloat();
-        mTempMin = in.readFloat();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        super.writeToParcel(dest, flags);
-        dest.writeFloat(mTempMax);
-        dest.writeFloat(mTempMin);
     }
 
     public float getTempMax() {
@@ -47,11 +33,50 @@ public class WeatherForecast extends WeatherCondition {
         mTempMin = value;
     }
 
-    @SuppressWarnings("unused")
+    public long getValidFrom() {
+        return mValidFrom;
+    }
+
+    public WeatherForecast setValidFrom(long validFrom) {
+        mValidFrom = validFrom;
+        return this;
+    }
+
+    public long getValidTo() {
+        return mValidTo;
+    }
+
+    public WeatherForecast setValidTo(long validTo) {
+        mValidTo = validTo;
+        return this;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        super.writeToParcel(dest, flags);
+        dest.writeFloat(this.mTempMax);
+        dest.writeFloat(this.mTempMin);
+        dest.writeLong(this.mValidFrom);
+        dest.writeLong(this.mValidTo);
+    }
+
+    protected WeatherForecast(Parcel in) {
+        super(in);
+        this.mTempMax = in.readFloat();
+        this.mTempMin = in.readFloat();
+        this.mValidFrom = in.readLong();
+        this.mValidTo = in.readLong();
+    }
+
     public static final Creator<WeatherForecast> CREATOR = new Creator<WeatherForecast>() {
         @Override
-        public WeatherForecast createFromParcel(Parcel in) {
-            return new WeatherForecast(in);
+        public WeatherForecast createFromParcel(Parcel source) {
+            return new WeatherForecast(source);
         }
 
         @Override
